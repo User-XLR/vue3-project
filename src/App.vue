ea9b26018884a000c0108a03cfe26c8e15f57127
Lurong.Xu <2624887@qq.com>
2025-02-06 16:07:53 +0800

    fix: 修改prettier的配置文件

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,33 @@
 <script setup lang="ts">
-import useBaseInfoStore from '@/stores/baseInfoStore'
-import layoutPage from '@/components/layouts/baseIndex.vue'
-import { ref, watchEffect, watch } from 'vue'
-import { useI18n } from 'vue-i18n'
-import { useRoute } from 'vue-router'
-const route = useRoute()
+import useBaseInfoStore from "@/stores/baseInfoStore";
+import layoutPage from "@/components/layouts/baseIndex.vue";
+import { ref, watchEffect, watch } from "vue";
+import { useI18n } from "vue-i18n";
+import { useRoute } from "vue-router";
+const route = useRoute();
 
-const { messages, locale }: any = useI18n()
-const baseInfoStore = useBaseInfoStore()
-const localeLang = ref(messages[baseInfoStore.language])
+const { messages, locale }: any = useI18n();
+const baseInfoStore = useBaseInfoStore();
+const localeLang = ref(messages[baseInfoStore.language]);
 const changeLanguage = () => {
-  locale.value = baseInfoStore.language
-  localeLang.value = messages.value[locale.value]
-}
-watchEffect(changeLanguage)
+	locale.value = baseInfoStore.language;
+	localeLang.value = messages.value[locale.value];
+};
+watchEffect(changeLanguage);
 watch(
-  () => route.query?.theme,
-  () => {
-    const { theme = 'light' }: any = route.query
-    if (baseInfoStore.getTheme.value) return
-    baseInfoStore.setTheme(theme)
-  }
-)
+	() => route.query?.theme,
+	() => {
+		const { theme = "light" }: any = route.query;
+		if (baseInfoStore.getTheme.value) return;
+		baseInfoStore.setTheme(theme);
+	}
+);
 </script>
 
 <template>
-  <el-config-provider
-    size="small"
-    :z-index="3000"
-    :locale="localeLang"
-  >
-    <layoutPage></layoutPage>
-  </el-config-provider>
+	<el-config-provider size="small" :z-index="3000" :locale="localeLang">
+		<layoutPage></layoutPage>
+	</el-config-provider>
 </template>
 
 <style scoped></style>
--- src/views/base/index.vue
+++ src/views/base/index.vue
@@ -1,54 +1,44 @@
 <template>
-  <div class="">{{ t('hello') }}</div>
-  <el-button
-    type="primary"
-    class="hover:bg-red-600"
-    @click="clickFn('en')"
-  >
-    {{ t('en') }}
-  </el-button>
-  <el-button @click="clickFn('zh')">{{ t('zh') }}</el-button>
-  <el-button @click="clickFn('hk')">{{ t('tw') }}</el-button>
-  <el-button @click="toTest">
-    {{ t('Jump') }}
-  </el-button>
-  <hr />
-  <el-switch
-    v-model="value3"
-    inline-prompt
-    active-text="light"
-    active-value="light"
-    inactive-value="dark"
-    inactive-text="dark"
-    @change="changeTheme"
-  />
-  <hr />
-  <div class="flex justify-center items-center bg-nav">
-    <div class="block m-5">
-      <span class="demonstration">{{ t('Default') }}</span>
-      <el-date-picker
-        v-model="value1"
-        type="datetime"
-        placeholder="Select date and time"
-      />
-    </div>
-  </div>
+	<div class="">{{ t("hello") }}</div>
+	<el-button type="primary" class="hover:bg-red-600" @click="clickFn('en')">
+		{{ t("en") }}
+	</el-button>
+	<el-button @click="clickFn('zh')">{{ t("zh") }}</el-button>
+	<el-button @click="clickFn('hk')">{{ t("tw") }}</el-button>
+	<el-button @click="toTest">{{ t("Jump") }} </el-button>
+	<hr />
+	<el-switch
+		v-model="value3"
+		inline-prompt
+		active-text="light"
+		active-value="light"
+		inactive-value="dark"
+		inactive-text="dark"
+		@change="changeTheme"
+	/>
+	<hr />
+	<div class="flex justify-center items-center bg-nav">
+		<div class="block m-5">
+			<span class="demonstration">{{ t("Default") }}</span>
+			<el-date-picker v-model="value1" type="datetime" placeholder="Select date and time" />
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
-import { useRouter, useRoute } from 'vue-router'
-import useBaseInfoStore from '@/stores/baseInfoStore'
+import { useRouter, useRoute } from "vue-router";
+import useBaseInfoStore from "@/stores/baseInfoStore";
 // import api from '@/api/baseApi'
-import { ref } from 'vue'
-import { useI18n } from 'vue-i18n'
-const router = useRouter()
-const route = useRoute()
-const baseInfoStore = useBaseInfoStore()
-const value3 = ref(baseInfoStore.getTheme)
+import { ref } from "vue";
+import { useI18n } from "vue-i18n";
+const router = useRouter();
+const route = useRoute();
+const baseInfoStore = useBaseInfoStore();
+const value3 = ref(baseInfoStore.getTheme);
 const clickFn = (str: any) => {
-  baseInfoStore.setLanguage(str)
-}
-const { t } = useI18n()
+	baseInfoStore.setLanguage(str);
+};
+const { t } = useI18n();
 /* api调用实例 */
 // api
 //   .baseA({
@@ -57,16 +47,16 @@ const { t } = useI18n()
 //   .then((res: any) => {
 //     console.log(res)
 //   })
-baseInfoStore.setToken('000')
+baseInfoStore.setToken("000");
 const toTest = () => {
-  const query = Object.assign({}, route.query)
-  router.push({ path: '/base/test', query })
-}
+	const query = Object.assign({}, route.query);
+	router.push({ path: "/base/test", query });
+};
 /* 切换主题 */
 const changeTheme = (val: any) => {
-  baseInfoStore.setTheme(val)
-}
-const value1 = ref('')
+	baseInfoStore.setTheme(val);
+};
+const value1 = ref("");
 </script>
 
 <style lang="scss" scoped></style>
