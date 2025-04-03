9e7fdccf6444221f0a739b07d2ef3259f4671978
User-XLR <86406274+User-XLR@users.noreply.github.com>
2025-04-03 14:17:02 +0800

    Update baseApi.ts

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -1,6 +1,6 @@
 import Request from "@/hooks/axios";
 
-const api = "https://neurongateway.dev2.neuroncloud.ai";
+const api = "";
 const baseA = (params: any) => {
 	return Request.get(`${api}/m/tenant`, params);
 };
